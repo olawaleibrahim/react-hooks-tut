1c23becb17f8c4a02e915384be1879889d116332
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2022-12-22 21:52:40 +0100

    useRef hook

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import StateTutorial from './UseState/StateTutorial';
 import ReducerTutorial from './UseReducer/ReducerTutorial';
 import EffectTutorial from './UseEffect/EffectTutorial';
+import RefTutorial from './UseRef/RefTutorial';
 
 function App() {
   return (
     <div className="App">
-      <StateTutorial />
+      {/* <StateTutorial />
       <ReducerTutorial />
-      <EffectTutorial />
+      <EffectTutorial /> */}
+      <RefTutorial />
 
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/UseRef/RefTutorial.js
@@ -0,0 +1,19 @@
+import React, { useRef } from "react";
+
+function RefTutorial() {
+    const inputRef = useRef();
+    const onClick = () => {
+        // inputRef.current.focus();
+        inputRef.current.value = "";
+    };
+
+    return (
+        <div>
+            <h1>Pedro</h1>
+            <input type="text" placeholder="Ex..." ref={inputRef} />
+            <button onClick={onClick}>Change Name</button>
+        </div>
+    );
+};
+
+export default RefTutorial;
